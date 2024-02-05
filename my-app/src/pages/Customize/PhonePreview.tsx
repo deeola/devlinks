3341a60fb245f61f586403799819434afadcae64
deeola <clazikar@gmail.com>
2024-02-05 12:42:12 +0100

    add image and condition it

--- my-app/src/pages/Customize/PhonePreview.tsx
+++ my-app/src/pages/Customize/PhonePreview.tsx
@@ -1,112 +1,6 @@
-// import React from "react";
-// import dp from "../../assets/images/icon-email.svg";
-// import "./PhonePreview.css";
-// import { linkArray } from "../../linkArray";
-// import arrow from "../../assets/images/icon-arrow-right.svg";
-// import { SBody } from "../../components/Text/Text";
-
-// import facebook from "../../assets/images/icon-facebook.svg";
-// import gitlab from "../../assets/images/icon-github.svg";
-// import x from "../../assets/images/icon-twitter.svg";
-
-
-// export default function PhonePreview() {
-//   console.log(dp);
-
-//   const arr = [
-//     { color: "#000000", image: gitlab, text: "Gitlab" },
-//     { color: "#4C4C4C", image: facebook, text: "Facebook" },
-//     { color: "#818181", image: x, text: "X" },
-//     { color: "#000000", image: gitlab, text: "Gitlab" },
-//     { color: "#4C4C4C", image: facebook, text: "Facebook" },
-//      { color: "#000000", image: gitlab, text: "Gitlab" }
-//   ];
-
-//   const shouldScroll = arr.length > 5;
-
-//   return (
-//     <div>
-//       <svg
-//         xmlns="http://www.w3.org/2000/svg"
-//         width="308"
-//         height="632"
-//         fill="none"
-//         viewBox="0 0 308 632"
-     
-//       >
-//         <path
-//           stroke="#737373"
-//           d="M1 54.5C1 24.953 24.953 1 54.5 1h199C283.047 1 307 24.953 307 54.5v523c0 29.547-23.953 53.5-53.5 53.5h-199C24.953 631 1 607.047 1 577.5v-523Z"
-//         />
-//         <path
-//           fill="#fff"
-//           stroke="#737373"
-//           d="M12 55.5C12 30.923 31.923 11 56.5 11h24C86.851 11 92 16.149 92 22.5c0 8.008 6.492 14.5 14.5 14.5h95c8.008 0 14.5-6.492 14.5-14.5 0-6.351 5.149-11.5 11.5-11.5h24c24.577 0 44.5 19.923 44.5 44.5v521c0 24.577-19.923 44.5-44.5 44.5h-195C31.923 621 12 601.077 12 576.5v-521Z"
-//         />
-//         <defs>
-//           <pattern id="image" x="0" y="0" width="100%" height="100%">
-//             <image x="0" y="0" width="100%" height="100%" xlinkHref={dp} />
-//           </pattern>
-//         </defs>
-
-//         <circle cx="153.5" cy="112" r="48" fill="url(#image)" />
-//         <rect width="160" height="16" x="73.5" y="185" fill="#EEE" rx="8" />
-//         <rect width="72" height="8" x="117.5" y="214" fill="#EEE" rx="4" />
-
-//         {arr.length === 0 ? (
-//           <>
-//             <rect width="237" height="44" x="35" y="278" fill="#EEE" rx="8" />
-//             <rect width="237" height="44" x="35" y="342" fill="#EEE" rx="8" />
-//             <rect width="237" height="44" x="35" y="406" fill="#EEE" rx="8" />
-//             <rect width="237" height="44" x="35" y="470" fill="#EEE" rx="8" />
-//             <rect width="237" height="44" x="35" y="534" fill="#EEE" rx="8" />
-//           </>
-//         ) : (
-
-
-//           arr.map((myarray, index) => (
-//             <foreignObject
-//               key={index}
-//               x="35"
-//               y={278 + index * 64} 
-//               width="237"
-//               height="44"
-//             >
-//               <div
-//                 className="linkTagContainer"
-//                 style={{ backgroundColor: myarray.color }}
-//               >
-//                 <div className="linkTagSubContainer">
-//                   <img
-//                     src={myarray.image}
-//                     alt="Imagee"
-//                     style={{ width: "100%"}}
-//                   />
-//                   <SBody className="labelText" text={myarray.text} />
-//                 </div>
-//                 <div>
-//                   <img
-//                     src={arrow}
-//                     alt="arrow"
-//                     style={{ width: "100%" }}
-//                   />
-//                 </div>
-//               </div>
-//             </foreignObject>
-//           ))
-
-
-
-
-//         )}
-//       </svg>
-//     </div>
-//   );
-// }
-
 
 import React from "react";
-import dp from "../../assets/images/icon-email.svg";
+import bwImage from "../../assets/images/mann.jpeg";
 import "./PhonePreview.css";
 import { linkArray } from "../../linkArray";
 import arrow from "../../assets/images/icon-arrow-right.svg";
@@ -117,7 +11,7 @@ import gitlab from "../../assets/images/icon-github.svg";
 import x from "../../assets/images/icon-twitter.svg";
 
 export default function PhonePreview() {
-  console.log(dp);
+
 
   const arr = [
     { color: "#000000", image: gitlab, text: "Gitlab", link: "www.gitlab.com" },
@@ -127,10 +21,12 @@ export default function PhonePreview() {
     { color: "#4C4C4C", image: facebook, text: "Facebook", link: "www.facebook.com"  },
   ];
 
-  const name = "Praise Bamigboye"
-  const email = "benwright@gmail.com"
+  let name: string | boolean = "Praise Bamigboye"
+  let email: string | boolean = "benwright@gmail.com"
+  let isImage: string | boolean = bwImage
 
   const shouldScroll = arr.length > 5;
+
 
   return (
     <div>
@@ -150,19 +46,37 @@ export default function PhonePreview() {
           stroke="#737373"
           d="M12 55.5C12 30.923 31.923 11 56.5 11h24C86.851 11 92 16.149 92 22.5c0 8.008 6.492 14.5 14.5 14.5h95c8.008 0 14.5-6.492 14.5-14.5 0-6.351 5.149-11.5 11.5-11.5h24c24.577 0 44.5 19.923 44.5 44.5v521c0 24.577-19.923 44.5-44.5 44.5h-195C31.923 621 12 601.077 12 576.5v-521Z"
         />
-        <defs>
+        {/* <defs>
           <pattern id="image" x="0" y="0" width="100%" height="100%">
-            <image x="0" y="0" width="100%" height="100%" xlinkHref={dp} />
+            <image x="0" y="0" width="100%" height="100%" xlinkHref={bwImage} />
           </pattern>
-        </defs>
+        </defs> */}
+        {
+            !isImage ?   <circle cx="153.5" cy="112" r="48" fill="#EEE" /> : <foreignObject x="100" y="64" width="100" height="100" className="rounded-image-foreign">
+            <div className="rounded-image-container">
+              <img src={bwImage} alt="Profile" className="rounded-image" />
+            </div>
+          </foreignObject>
+        }
 
-        <circle cx="153.5" cy="112" r="48" fill="url(#image)" />
-        <foreignObject x="0" y="180" className="nameEmailForeignObject">
-          <MBody className="textFONAME"  text={name} />
-        </foreignObject>
-        <foreignObject x="0" y="220" className="nameEmailForeignObject">
-          <SBody className="textFO"  text={email} />
-        </foreignObject>
+       
+
+        
+
+
+        {
+            !name  ?  <rect width="160" height="16" x="73.5" y="185" fill="#EEE" rx="8" /> : <foreignObject x="0" y="180" className="nameEmailForeignObject">
+            <MBody className="textFONAME"  text={name} />
+          </foreignObject>
+        }
+
+        {
+            !email ? <rect width="160" height="16" x="73.5" y="207" fill="#EEE" rx="8" /> :  <foreignObject x="0" y="220" className="nameEmailForeignObject">
+            <SBody className="textFO"  text={email} />
+          </foreignObject>
+        }
+        
+       
   
 
         {arr.length === 0 ? (
