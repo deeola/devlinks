adade18f1d2d93c4172f50ce758626625c5b3004
deeola <clazikar@gmail.com>
2024-02-09 11:54:53 +0100

    add addewlink

--- my-app/src/components/Addlink/Addnewlink.tsx
+++ my-app/src/components/Addlink/Addnewlink.tsx
@@ -3,6 +3,7 @@ import { linkArray } from "../../linkArray";
 import "../Dropdown/Dropdown.css";
 import "../Input/InputField.css";
 import iconLink from "../../assets/images/icon-link.svg";
+import { MBody } from "../Text/Text";
 
 type Tdropdown = {
   dropArrayImage: string;
@@ -176,73 +177,69 @@ export default function AddnewLink(Props: Tdropdown) {
   //   }
 
 
-
-  return (
+return (
     <div>
-      <fieldset className="flex flex-col gap-2 border py-1 px-4">
-        <legend className="text-2xl font-semibold mb-2 text-gray-500">
-          Prompts
-        </legend>
-        {prompts.map((prompt, i) => (
-          <div key={prompt.timestamp}>
+        <div onClick={handleAddPrompt} >Add Prompt</div>
+      {prompts.map((prompt, i) => (
+        <div className="addlink-container" key={prompt.timestamp}>
+          <div className="addlinknumber-remove">
+            <MBody text={`= Link #${prompts.indexOf(prompt) + 1}`} />
+            <button onClick={() => handleDelete(i)}> Remove </button>
+          </div>
+          <div>
             <div>
-              <div
-                className={`custom-select ${i === activeIndex ? "active" : ""}`}
-              >
-                <button
-                  className="select-button"
-                  role="combobox"
-                  aria-labelledby="select button"
-                  aria-haspopup="listbox"
-                  aria-expanded={i === activeIndex ? "true" : "false"}
-                  aria-controls="select-dropdown"
-                  onClick={() => handleButtonClick(i)}
-                >
-                  <div className="dropdown-selected-value">
-                    <span className="image">
-                      <img src={dropArrayImage} alt="Icon" />
-                    </span>
-
-                    <span className="selected-value">{prompt.label}</span>
-                  </div>
-
-                  <span className="arrow"></span>
-                </button>
-                <ul
-                  className="select-dropdown"
-                  role="listbox"
-                  id="select-dropdown"
-                >
-                  {linkArray.map((list, index) => (
-                    <li
-                      key={index}
-                      role="option"
-                      onClick={(e) =>
-                        handleOptionClick(
-                          e,
-                          i,
-                          list.label,
-                          list.image,
-                          list.placeholder,
-                          list.bgColor,
-                          list.id
-                        )
-                      }
-                    >
-                      <input type="radio" id={list.label} name={list.label} />
-                      <label htmlFor={list.label}>
-                        <img
-                          className="bx bxl-github"
-                          src={list.image}
-                          alt=""
-                        />
-                        {list.label}
-                      </label>
-                    </li>
-                  ))}
-                </ul>
+              <label className="label">Platform</label>
+              <div className="platform-link">
+                <div className={`custom-select ${i === activeIndex ? "active" : ""}`}>
+                  <button
+                    className="select-button"
+                    role="combobox"
+                    aria-labelledby="select button"
+                    aria-haspopup="listbox"
+                    aria-expanded={i === activeIndex ? "true" : "false"}
+                    aria-controls="select-dropdown"
+                    onClick={() => handleButtonClick(i)}
+                  >
+                    <div className="dropdown-selected-value">
+                      <span className="image">
+                        <img src={dropArrayImage} alt="Icon" />
+                      </span>
+                      <span className="selected-value">{prompt.label}</span>
+                    </div>
+                    <span className="arrow"></span>
+                  </button>
+                  <ul className="select-dropdown" role="listbox" id="select-dropdown">
+                    {linkArray.map((list, index) => (
+                      <li
+                        key={index}
+                        role="option"
+                        onClick={(e) =>
+                          handleOptionClick(
+                            e,
+                            i,
+                            list.label,
+                            list.image,
+                            list.placeholder,
+                            list.bgColor,
+                            list.id
+                          )
+                        }
+                      >
+                        <input type="radio" id={list.label} name={list.label} />
+                        <label htmlFor={list.label}>
+                          <img className="bx bxl-github" src={list.image} alt="" />
+                          {list.label}
+                        </label>
+                      </li>
+                    ))}
+                  </ul>
+                </div>
               </div>
-
+            </div>
+          </div>
+          <div>
+            <label className="label">Link</label>
+            <div className="platform-link">
               <div className={`input-container ${error ? "error" : ""}`}>
                 <span className="image">
                   <img src={iconLink} alt="Icon" />
@@ -261,28 +258,15 @@ export default function AddnewLink(Props: Tdropdown) {
                   {/* {touched && error && <span className="error-span"> {errorMessage} </span>} */}
                 </div>
               </div>
-
-              <button
-                className="border bg-red-400 py-2.5 px-4 rounded-lg text-white font-bold text-xl"
-                type="button"
-                onClick={() => handleDelete(i)}
-              >
-                —
-              </button>
             </div>
           </div>
-        ))}
-
-        <div className="w-full flex justify-center">
-          <button
-            className="border bg-indigo-400 py-1 px-2 rounded-lg text-white font-bold text-xl"
-            type="button"
-            onClick={handleAddPrompt}
-          >
-            Add Prompt
-          </button>
         </div>
-      </fieldset>
+      ))}
     </div>
   );
-}
+    }
+   
+
+
+{/* {touched && error && <span className="error-span"> {errorMessage} </span>} */}
+// onClick={handleAddPrompt}
