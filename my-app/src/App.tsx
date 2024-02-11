d0716ded067d73b9859443e9ad9fd8e3629c2bd0
deeola <clazikar@gmail.com>
2024-02-12 00:05:42 +0100

    add preview card

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,10 +1,12 @@
 import "./App.css";
 import Customize from "./pages/Customize/Customize";
+import Preview from "./pages/Preview/Preview";
 
 function App() {
   return (
     <div className="App">
-      <Customize />
+      {/* <Customize /> */}
+      <Preview />
     </div>
   );
 }
--- my-app/src/pages/Preview/Preview.tsx
+++ my-app/src/pages/Preview/Preview.tsx
@@ -2,44 +2,51 @@ import React from "react";
 import "./Preview.css";
 import Button from "../../components/Button/Button";
 import { MBody, SBody } from "../../components/Text/Text";
+import arrow from "../../assets/images/icon-arrow-right.svg";
+import github from "../../assets/images/icon-github.svg";
+import twitter from "../../assets/images/icon-twitter.svg";
+import linkedin from "../../assets/images/icon-linkedin.svg";
+import profileimage from "../../assets/images/mann.jpeg";
 
 export default function Preview() {
   return (
     <div className="preview-container">
       <div className="preview-Header">
         <div className="preview-navbar-container">
-          <Button text="Back to Editor" />
-          <Button text="Share Link" />
+          <Button text="Back to Editor" classname="preview-navbar-link" />
+          <Button text="Share Link" classname="preview-navbar-link"  />
         </div>
       </div>
       <div className="preview-body">
         <div className="preview-card-container">
           <div className="preview-card-user-details">
-            <img className="oreview-img" src="" alt="display-img" />
-            <MBody text="Ben Wright" />
-            <SBody text="ben@example.com" />
+            <div className="profile-image-container">
+            <img className="preview-img" src={profileimage} alt="display-img" />
+            </div>
+            <MBody text="Ben Wright" className="preview-username" />
+            <SBody text="ben@example.com" className="preview-email" />
           </div>
           <div className="preview-cards">
             <div className="cards">
-              <div>
-                <img className="preview-card-icon" src="" alt="card-img" />
-                <p>Github</p>
+              <div className="preview-card-icon-name-container">
+                <img className="preview-card-icon" src={github} alt="card-img" />
+                <MBody text="Github" className="card-name"/>
               </div>
-              <img className="card-arrow" src="" alt="card-img" />
+              <img className="card-arrow" src={arrow} alt="card-img" />
             </div>
             <div className="cards">
-              <div>
-                <img className="preview-card-icon" src="" alt="card-img" />
-                <p>Twitter</p>
+              <div className="preview-card-icon-name-container">
+                <img className="preview-card-icon" src={twitter} alt="card-img" />
+                <MBody text="Twitter" className="card-name" />
               </div>
-              <img className="card-arrow" src="" alt="card-img" />{" "}
+              <img className="card-arrow" src={arrow} alt="card-img" />
             </div>
             <div className="cards">
-              <div>
-                <img className="preview-card-icon" src="" alt="card-img" />
-                <p>Linkedin</p>
+              <div className="preview-card-icon-name-container">
+                <img className="preview-card-icon" src={linkedin} alt="card-img" />
+                <MBody text="Linkedin" className="card-name" />
               </div>
-              <img className="card-arrow" src="" alt="card-img" /></div>
+              <img className="card-arrow" src={arrow} alt="card-img" /></div>
           </div>
         </div>
       </div>
